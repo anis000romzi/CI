d1ecd83914fbf2d6d1732ce63826274f12056672
Anis <anis000romzi@gmail.com>
2022-03-22 16:38:14 +0700

    init

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Muhammad Anis Romzi
+            Saya tingga di daerah Jalan Talang
+            kegiatan yang saya sukai adalah
+            menonton film";
+    }
+}
